dfa8b64e0c7107fd33eb14e9d1c422f3cc9e30f5
Alexey <avorobyev174@gmail.com>
2023-12-28 16:21:47 +0500

    инициализация react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
+
+
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={ classNames('app', { selectable: true, hover: true }, [ theme, 'cls2']) }>
+      <button onClick={ toggleTheme }>TOGGLE</button>
       <Link to={ '/' }>Главная</Link>
       <Link to={ '/about '}>О сайте</Link>
-
-        <Suspense fallback={ <div>Loading...</div> }>
-          <Routes>
-            <Route path={ '/about' } element={ <AboutPageAsync/> }></Route>
-            <Route path={ '/' } element={ <MainPageAsync/> }></Route>
-          </Routes>
-        </Suspense>
+      <Suspense fallback={ <div>Loading...</div> }>
+        <Routes>
+          <Route path={ '/about' } element={ <AboutPageAsync/> }></Route>
+          <Route path={ '/' } element={ <MainPageAsync/> }></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -1,6 +1,5 @@
 import { lazy } from 'react';
 
-
 export const AboutPageAsync = lazy(
   // @ts-ignore
   () => new Promise(resolve => { setTimeout(() => resolve(import('./AboutPage')), 1000) }));

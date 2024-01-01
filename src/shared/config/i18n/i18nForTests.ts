f65659d795940df76808c0ada199014215f93d92
Alexey <avorobyev174@gmail.com>
2024-01-01 15:50:58 +0500

    fix loki target

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -4,13 +4,13 @@ import { initReactI18next } from 'react-i18next';
 void i18n
   .use(initReactI18next)
   .init({
-    lng: 'en',
-    fallbackLng: 'en',
+    lng: 'ru',
+    fallbackLng: 'ru',
     debug: false,
     interpolation: {
       escapeValue: false // not needed for react!!
     },
-    resources: { en: { translations: {} } }
+    resources: { ru: { translationsNS: {} } }
   });
 
 export default i18n;
